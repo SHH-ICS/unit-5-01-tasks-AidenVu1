9a4a27f455981675401397ae93f2362dcb4ed923
Aiden Vu <100857445+AidenVu1@users.noreply.github.com>
2023-05-02 10:06:20 -0400

    Update Q4Picture.py

--- Q4Picture.py
+++ Q4Picture.py
@@ -1,2 +1,35 @@
-# Using the pygame library, draw a simple picture. 
-# It can be anything you like, but you must use at least 2 different types of shapes and 3 different colors.
+import pygame
+
+pygame.init()
+
+size = (500, 400)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Smiley Face")
+
+yellow = (255, 255, 0)
+black = (0, 0, 0)
+red = (255, 0, 0)
+blue = (35, 206, 235)
+
+screen.fill(blue)
+
+pygame.draw.circle(screen, yellow, (200, 200), 100)
+
+pygame.draw.circle(screen, red, (150, 150), 20)
+pygame.draw.circle(screen, red, (250, 150), 20)
+
+pygame.draw.arc(screen, black, (150, 150, 100, 100), 3.14, 6.28)
+
+pygame.draw.circle(screen, yellow, (80, 220), 20)
+pygame.draw.circle(screen, yellow, (320, 220), 20)
+
+pygame.display.flip()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
+
